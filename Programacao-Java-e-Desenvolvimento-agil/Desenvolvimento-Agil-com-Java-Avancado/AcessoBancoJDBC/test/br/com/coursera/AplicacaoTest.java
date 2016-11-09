f4d51cd0a2513a68b98a677d98d648fad93ef60c
marcelohb <marcelobattaglion@bol.com.br>
2016-11-09 17:17:48 -0200

    Conclusão dos testes com DBUnit

--- Programacao-Java-e-Desenvolvimento-agil/Desenvolvimento-Agil-com-Java-Avancado/AcessoBancoJDBC/test/br/com/coursera/AplicacaoTest.java
+++ Programacao-Java-e-Desenvolvimento-agil/Desenvolvimento-Agil-com-Java-Avancado/AcessoBancoJDBC/test/br/com/coursera/AplicacaoTest.java
@@ -3,6 +3,7 @@ package br.com.coursera;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.dbunit.Assertion;
 import org.dbunit.JdbcDatabaseTester;
@@ -19,10 +20,15 @@ public class AplicacaoTest {
 	
 	private UsuarioDAO dao;
 	private JdbcDatabaseTester jd;
+
+//	private static String url = "jdbc:mysql://localhost/coursera";
+	private static String url = "jdbc:mysql://192.168.99.100:32769/coursera";
+	private static String user = "root";
+	private static String pass = "vertrigo";
 	
 	@Before
 	public void setUp() throws Exception {
-		jd = new JdbcDatabaseTester("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/coursera","root","vertrigo");
+		jd = new JdbcDatabaseTester("com.mysql.jdbc.Driver",url,user,pass);
 		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
 		jd.setDataSet(loader.load("/inicio.xml"));
 		jd.onSetup();
@@ -46,6 +52,28 @@ public class AplicacaoTest {
 		ITable currentTable = currentDataSet.getTable("usuario");
 		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
 		IDataSet expectedDataSet = loader.load("/verifica.xml");
+		ITable expectedTable = expectedDataSet.getTable("usuario");
+		Assertion.assertEquals(expectedTable, currentTable);
+	}
+	
+	@Test
+	public void verificaRanking() throws SQLException, Exception {
+
+		List<Usuario> ranking = dao.ranking();
+		
+		assertEquals("jonh", ranking.get(0).getLogin());
+		assertEquals("eu", ranking.get(1).getLogin());
+		assertEquals("danilo", ranking.get(2).getLogin());
+	}
+	
+	@Test
+	public void adicionarPontos() throws SQLException, Exception {
+		dao.adicionarPontos("eu", 10);
+		
+		IDataSet currentDataSet = jd.getConnection().createDataSet();
+		ITable currentTable = currentDataSet.getTable("usuario");
+		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
+		IDataSet expectedDataSet = loader.load("/verificaPontos.xml");
 		ITable expectedTable = expectedDataSet.getTable("usuario");
 		Assertion.assertEquals(expectedTable, currentTable);
 	}
